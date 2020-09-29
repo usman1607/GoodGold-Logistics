c4c39355cffdd3aa51651c55c5bc1712bf8661d8
Usman <usman1607@yahoo.com>
2020-09-29 16:10:40 -0700

    correct after login

--- src/main/java/com/goodgold/logistics/security/SecurityConfig.java
+++ src/main/java/com/goodgold/logistics/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                // .loginProcessingUrl("/loginSecure")
-                .defaultSuccessUrl("/index", true)
+                .defaultSuccessUrl("//https://goodgoldlogistics.herokuapp.com", true)
               //  .usernameParameter("username").passwordParameter("password")
                 //.failureUrl("/login.html?error=true")
                 //.failureHandler(authenticationFailureHandler())
