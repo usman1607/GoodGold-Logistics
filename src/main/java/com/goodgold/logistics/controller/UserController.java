beb2d3115e299653f9cd962433d5c3697d053b31
Usman <usman1607@yahoo.com>
2020-10-01 10:24:26 -0700

    yesssssssss

--- src/main/java/com/goodgold/logistics/controller/UserController.java
+++ src/main/java/com/goodgold/logistics/controller/UserController.java
@@ -100,9 +100,9 @@ public class UserController {
         return "user/SellersList";
     }
 
-    @RequestMapping(value = "/myPage/page", method = RequestMethod.GET)
-    public String myPage(){
-//        model.addAttribute("sellers", userRepository.findUsersByTitleEquals("Seller"));
+    @RequestMapping(value = "/myPage/page/{username}", method = RequestMethod.GET)
+    public String myPage(@PathVariable("username") String username, Model model){
+        model.addAttribute("user", userRepository.findUserByUsername(username));
         return "admin/dashboard";
     }
 
