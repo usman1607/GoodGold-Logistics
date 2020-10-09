83cbfa747ff8465b4e4643058066150215699e92
Usman <usman1607@yahoo.com>
2020-10-09 10:56:51 -0700

    Read me

--- src/main/resources/static/js/myJsFile.js
+++ src/main/resources/static/js/myJsFile.js
@@ -5,7 +5,6 @@ function congrats(){
             Swal.fire({
                 icon: 'success',
                 title: 'Congratulations! You have successfully registered.',
-                showCloseButton: true,
                 showCancelButton: false,
                 focusConfirm: false,
                 html:
@@ -19,77 +18,4 @@ function congrats(){
             })
         }
     })
-}
-//
-// function congrats(){
-//     Swal.fire({
-//         icon: 'success',
-//         title: 'Congratulations! You have successfully registered.',
-//         showConfirmButton: false,
-//         timer: 9000
-//     })
-// }
-
-function deleteSeller() {
-    Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-            )
-        }
-    })
-}
-
-$('.confirmation').click(function (e) {
-    let href = $(this).attr('href');
-
-    swal({
-            title: "Are you sure?",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, delete it!",
-            cancelButtonText: "No, cancel plx!",
-            closeOnConfirm: true,
-            closeOnCancel: true
-        },
-        function (isConfirm) {
-            if (isConfirm) {
-                window.location.href = href;
-            }
-        });
-
-    return false;
-});
-
-function confirmation(){
-    let href = $(this).attr('href');
-
-    swal({
-            title: "Are you sure?",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, delete it!",
-            cancelButtonText: "No, cancel plx!",
-            closeOnConfirm: true,
-            closeOnCancel: true
-        },
-        function (isConfirm) {
-            if (isConfirm) {
-                window.location.href = href;
-            }
-        });
-
-    return false;
 }
