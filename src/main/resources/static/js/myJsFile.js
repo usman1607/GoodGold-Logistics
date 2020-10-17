2dcb44e95e7ef149ea334b64e85c3908e1629024
Usman <usman1607@yahoo.com>
2020-10-17 13:29:01 -0700

    new update

--- src/main/resources/static/js/myJsFile.js
+++ src/main/resources/static/js/myJsFile.js
@@ -20,23 +20,25 @@ function congrats(){
     })
 }
 
-function created(){
-    $(document).ready(function () {
-        let action = document.getElementById('staff-added');
-        if (action.innerHTML === "You have successfully added a staff") {
-            Swal.fire({
-                icon: 'success',
-                title: 'Congrats! You have successfully added a staff.',
-                showCancelButton: false,
-                focusConfirm: false,
-                confirmButtonColor: '#b1ef95',
-                confirmButtonText:
-                    '<a href="/staff/list"><i class="fa fa-thumbs-up"></i> Great!</a>',
-                confirmButtonAriaLabel: 'Thumbs up, great!'
-            })
-        }
-    })
-}
+
+    // $(document).ready(function () {
+    //     $("#createStaff").click(function (e) {
+    //         let action = document.getElementById('staff-added');
+    //         if (action.innerHTML === "You have successfully added a staff") {
+    //             Swal.fire({
+    //                 icon: 'success',
+    //                 title: 'Congrats! You have successfully added a staff.',
+    //                 showCancelButton: false,
+    //                 focusConfirm: false,
+    //                 confirmButtonColor: '#b1ef95',
+    //                 confirmButtonText:
+    //                     '<a href="/staff/list"><i class="fa fa-thumbs-up"></i> Great!</a>',
+    //                 confirmButtonAriaLabel: 'Thumbs up, great!'
+    //             });
+    //         }
+    //     });
+    // });
+
 
 
     $(document).ready(function () {
@@ -47,3 +49,33 @@ function created(){
             });
         });
     });
+
+
+    // $(document).ready(function () {
+    //     $(".sellerDel").click(function (e) {
+    //         e.preventDefault();
+    //         // let href = $(this).attr("href");
+    //         swal.fire({
+    //                 title: "Are you sure?",
+    //                 type: "warning",
+    //                 showCancelButton: true,
+    //                 confirmButtonColor: "#DD6B55",
+    //                 confirmButtonText: "Yes, delete it!",
+    //                 cancelButtonText: "No, cancel plx!",
+    //                 closeOnConfirm: true,
+    //                 closeOnCancel: true
+    //             }).then((result) => {
+    //             if (result.isConfirmed) {
+    //                 window.location.href = this.href;
+    //                 Swal.fire(
+    //                     'Deleted!',
+    //                     'Your file has been deleted.',
+    //                     'success'
+    //                 )
+    //             }
+    //         });
+    //         return false;
+    //     });
+    // });
+
+
